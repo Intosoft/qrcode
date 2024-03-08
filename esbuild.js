c45396736da2236d4e0e03a7202015a7c9179038
Sakul Budhathoki <sakulbudhathoki977@gmail.com>
2024-03-08 21:38:56 +0545

    fix: standalone build

--- esbuild.js
+++ esbuild.js
@@ -1,13 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable import/no-extraneous-dependencies */
 const { build } = require('esbuild');
-const { Generator } = require('npm-dts');
 const { dependencies } = require('./package.json');
-
-new Generator({
-    entry: 'src/index.ts',
-    output: 'dist/index.d.ts',
-}).generate();
 
 const sharedConfig = {
     entryPoints: ['src/index.ts'],
